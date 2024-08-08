0a6369ba62d30c269de4c36d9dc4483ae6cc3490
thanhsteven <kimthanhpham96@gmail.com>
2024-08-08 10:35:33 +0700

    Update source code

--- wp-content/themes/woodmart-child/parts/thanh-html-block.php
+++ wp-content/themes/woodmart-child/parts/thanh-html-block.php
@@ -1,7 +1,15 @@
 <?php
 // Header H1 for every page
 function headerH1() {
-  return "<h1 class='thanh-header-h1'>Hello Word !</h1>";
+  $current_page_id = get_queried_object_id();
+  switch ($current_page_id) {
+    case 13:
+      echo "<h1 class='thanh-header-h1'>Đây là trang chủ</h1>";
+      break;
+    default:
+      echo "<p class='thanh-header-h1'>Trang con</p>";
+      break;
+  }
 }
 add_shortcode('thanh_header_H1','headerH1');
 ?>
